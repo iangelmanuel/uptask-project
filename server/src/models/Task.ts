99c98f0e749aee549ed8288bc8a98e1e6e051d1d
iAngelManuel <iangelmanueldm@gmail.com>
2024-05-21 00:27:19 -0500

    refactor: Add task status field to Task model

--- server/src/models/Task.ts
+++ server/src/models/Task.ts
@@ -1,10 +1,21 @@
 import mongoose, { Schema, Document, Types } from 'mongoose'
 
+export type TaskStatus = (typeof taskStatus)[keyof typeof taskStatus]
+
 export interface ITask extends Document {
   name: string
   description: string
   project: Types.ObjectId
+  status: TaskStatus
 }
+
+const taskStatus = {
+  PENDING: 'PENDING',
+  ON_HOLD: 'ON_HOLD',
+  IN_PROGRESS: 'IN_PROGRESS',
+  UNDER_REVIEW: 'UNDER_REVIEW',
+  COMPLETED: 'COMPLETED',
+} as const
 
 const TaskSchema = new Schema(
   {
@@ -22,6 +33,11 @@ const TaskSchema = new Schema(
       type: Types.ObjectId,
       ref: 'Project',
       required: true,
+    },
+    status: {
+      type: String,
+      enum: Object.values(taskStatus),
+      default: taskStatus.PENDING,
     },
   },
   { timestamps: true },
