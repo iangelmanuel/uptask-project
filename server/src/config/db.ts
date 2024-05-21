2c9e7a633ce6c0c0345ef1f26a1418532e7f830f
iAngelManuel <iangelmanueldm@gmail.com>
2024-05-20 22:46:44 -0500

    chore: Add MongoDB connection configuration

---
+++ server/src/config/db.ts
@@ -0,0 +1,14 @@
+import colors from 'colors'
+import mongoose from 'mongoose'
+import { exit } from 'node:process'
+
+export default async function connectDB() {
+  try {
+    const connection = await mongoose.connect(process.env.DB_URL)
+    const url = `${connection.connection.host}/${connection.connection.port}`
+    console.log(colors.blue.bold(`MongoDB connected: ${url}`))
+  } catch (error) {
+    console.log(error.message)
+    exit(1)
+  }
+}
