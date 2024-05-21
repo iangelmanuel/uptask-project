670e2b7eb033512394ec605ff9bfdd7290555b88
iAngelManuel <iangelmanueldm@gmail.com>
2024-05-20 22:46:52 -0500

    refactor: Update server.ts to use explicit type for app variable

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
-import server from './server'
 import colors from 'colors'
+import server from './server'
 
 // PORT
 const port = process.env.PORT || 4000
 
 server.listen(port, () => {
-  console.log(colors.magenta.bold(`Server is running on http://localhost:${port}`))
+  console.log(
+    colors.magenta.bold(`Server is running on http://localhost:${port}`),
+  )
 })
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,9 @@
+import dotenv from 'dotenv'
 import express, { Application } from 'express'
+import connectDB from './config/db'
+
+dotenv.config()
+connectDB()
+
 const app: Application = express()
 export default app
