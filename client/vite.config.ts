cf9c9c8ef9f3f58d36a34d1cab2b204c2140d171
iAngelManuel <iangelmanueldm@gmail.com>
2024-05-21 23:48:34 -0500

    chore: Update TypeScript configuration and Vite configuration for path aliases

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
