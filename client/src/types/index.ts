bec8cc542e7a71a174062ce3f2aff32b80d6af30
iAngelManuel <iangelmanueldm@gmail.com>
2024-05-23 00:06:13 -0500

    feat: Add dashboardProjectSchema for dashboard project data

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -7,6 +7,15 @@ export const projectSchema = z.object({
   description: z.string(),
 })
 
+export const dashboardProjectSchema = z.array(
+  projectSchema.pick({
+    _id: true,
+    projectName: true,
+    clientName: true,
+    description: true,
+  }),
+)
+
 export type Project = z.infer<typeof projectSchema>
 export type ProjectFormData = Pick<
   Project,
