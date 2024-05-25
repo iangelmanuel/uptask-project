4eecd47022b3a34d3793b966d5e658d09bc76d7e
iAngelManuel <iangelmanueldm@gmail.com>
2024-05-25 03:11:56 -0500

    feat: Add Spanish translations for task statuses

---
+++ client/src/locales/es.ts
@@ -0,0 +1,7 @@
+export const statusTraslations: { [key: string]: string } = {
+  pending: 'Pendiente',
+  onHold: 'En espera',
+  inProgress: 'En progreso',
+  underReview: 'En revisión',
+  completed: 'Completado',
+}
