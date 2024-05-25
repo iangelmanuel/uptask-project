731a0d8e79647b58bed86816124b7ba0a5a1d1ad
iAngelManuel <iangelmanueldm@gmail.com>
2024-05-25 04:25:25 -0500

    chore: Update corsConfig to include API whitelist for development mode

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -4,6 +4,10 @@ export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
     const whiteList = [process.env.FRONDEND_URL]
 
+    if (process.argv[2] === '--api') {
+      whiteList.push(undefined)
+    }
+
     if (whiteList.includes(origin)) {
       callback(null, true)
     } else {
