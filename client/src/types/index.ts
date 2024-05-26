55b85e16a7464fc56d519dbdbe566a3746aecf6b
iAngelManuel <iangelmanueldm@gmail.com>
2024-05-26 05:46:06 -0500

    feat: Update authentication types and schemas

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,5 +1,32 @@
 import { z } from 'zod'
 
+// AUTH & USER //
+const authSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+  passwordConfirmation: z.string(),
+  token: z.string(),
+})
+
+export type Auth = z.infer<typeof authSchema>
+
+export type UserLoginForm = Pick<Auth, 'email' | 'password'>
+export type UserRegistrationForm = Pick<
+  Auth,
+  'name' | 'email' | 'password' | 'passwordConfirmation'
+>
+export type RequestConfirmationCodeForm = Pick<Auth, 'email'>
+
+export type ConfirmToken = Pick<Auth, 'token'>
+
+export type ForgotPasswordForm = Pick<Auth, 'email'>
+
+export type NewPasswordForm = Pick<
+  Auth,
+  'password' | 'passwordConfirmation'
+>
+
 // TASKS //
 
 export type TaskTatus =
