80d51fc2c939708bf183aa4a9e27ac7742250cf4
iAngelManuel <iangelmanueldm@gmail.com>
2024-05-26 21:32:56 -0500

    feat: Add request interceptor to include authorization token

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -3,3 +3,13 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: `${import.meta.env.VITE_BACKEND}`,
 })
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('AUTH_TOKEN')
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
