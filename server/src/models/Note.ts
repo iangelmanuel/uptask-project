5039ffce72525e97e2d8a06fc84514235e5beea3
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-03 15:59:56 -0500

    refactor: Update `Note` model schema to include createdBy field
    This commit updates the `Note` model schema in the `Note.ts` file to include the `createdBy` field. The `createdBy` field is of type `Types.ObjectId` and is required. It references the `User` model and represents the user who created the note. This change improves the data structure and allows for better tracking of note creators.

--- server/src/models/Note.ts
+++ server/src/models/Note.ts
@@ -6,22 +6,25 @@ export interface INote extends Document {
   task: Types.ObjectId
 }
 
-const NoteSchema: Schema = new Schema({
-  content: {
-    type: String,
-    required: true,
+const NoteSchema: Schema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    task: {
+      type: Types.ObjectId,
+      ref: 'Task',
+      required: true,
+    },
   },
-  createdBy: {
-    type: Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  task: {
-    type: Types.ObjectId,
-    ref: 'Task',
-    required: true,
-  },
-})
+  { timestamps: true },
+)
 
 const Note = mongoose.model<INote>('Note', NoteSchema)
 export default Note
